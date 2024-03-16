cf655868a4b3f1f2ff0eae7df0a165caf925b024
Abdelrhmankaram <abdelrhmankaram171@gmail.com>
2024-03-16 03:45:52 +0200

    ahm

---
+++ redirect.js
@@ -0,0 +1,8 @@
+// ضيفو الكود في قبل فتحته التاج بتاعت ال body
+<script>
+    // Example JavaScript code for device detection
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // Redirect mobile users to a different page
+        window.location.href = "mobile.html"
+    }
+</script>
